ae235ee53492f8365576c6d4d71e3c0a05b6d442
martinsander00 <msanderh@gmail.com>
2022-09-24 16:00:50 -0600

    primer intento de circuito en simulide

--- labs/lab2/src/lab2.c
+++ labs/lab2/src/lab2.c
@@ -60,5 +60,107 @@ int main(void)
   }
 }
 
+void fsm(){
+    switch (state)
+    {
+    case PV_blink:
+        delay = 30;  // Parpadeo intermitente
+        button = 0;
+        if (valid){
+            PORTB ^= (1 << PB3);
+        }
+        if (blink == 6){       // 6 blinks
+            nxt_state = LDVD;
+            count = 0;
+            blink = 0;
+        }
+        break;
+
+    case LDVD:      // STOP vehicular
+        delay = 61;
+        PORTB &= ~(1 << PB3);
+        PORTB |= (1 << PB1);
+        if (blink == 1){
+            count = 0;
+            blink = 0;
+            nxt_state = LDPP;
+        }
+        break;
+
+    case LDPV:      // Paso vehicular
+        PORTB |= (1 << PB3);
+        PORTB |= (1 << PB5)|(1 << PB7); // LDPD ON
+        PORTB &= ~(1 << PB1);  // LDVD OFF
+        delay = 610;
+        if (button == 1 && blink > 0){
+            nxt_state = PV_blink;
+            count = 0;
+            blink = 0;
+        } 
+        break;
+
+    case LDPP:      // Paso peatonal
+        delay = 610;
+        PORTB |= (1 << PB4)|(1 << PB6);
+        PORTB &= ~(1 << PB5);
+        PORTB &= ~(1 << PB7);  // LDPD OFF
+        if (blink == 1){
+            nxt_state = PP_blink;
+            count = 0;
+            blink = 0;
+        }
+        break;
+
+    case LDPD:
+        delay = 70;
+        PORTB &= ~(1 << PB4);
+        PORTB &= ~(1 << PB6);
+        PORTB |= (1 << PB5)|(1 << PB7); // Luces peatonales en rojo
+        if (blink == 1){
+            nxt_state = LDPV;
+            count = 0;
+            blink = 0;
+        }
+        break;
+
+    case PP_blink:      // paso peatones apunto de terminar
+        delay = 30;
+        if (valid){
+            PORTB ^= (1 << PB4);//|~(1 << PB6);
+            PORTB ^= (1 << PB6);//|~(1 << PB4);
+        }
+        if (blink == 6){
+            nxt_state = LDPD;
+            count = 0;
+            blink = 0;
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+ISR(TIMER0_OVF_vect){
+    if(count == delay){
+        valid = 1;  // pasó tiempo necesario y se puede cambiar luz
+        blink++;   // cantidad de cambios de luz ++
+        count = 0;
+    } else{
+        count++;
+        valid = 0;  // no cambiar luz
+    }
+}
 
 
+ISR(INT0_vect){
+    if (state == LDPV){
+    	button = 1;
+    }
+}
+
+ISR(INT1_vect){
+    if (state == LDPV){
+    	button = 1;
+    }
+}
